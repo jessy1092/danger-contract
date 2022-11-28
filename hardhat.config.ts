3451def1419859aad8645e13d8b97fc7b95fdd35
Lee <jessy1092@gmail.com>
2022-11-28 20:00:06 +0800

    fix: Setup .env example

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,7 @@ import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
 import * as dotenv from 'dotenv';
 
-dotenv.config({ path: '../../.env' });
+dotenv.config({ path: '.env' });
 
 const config: HardhatUserConfig = {
 	solidity: '0.8.17',
